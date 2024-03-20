f31b56a6dbf8f74aa5176bf979d26078673d7da1
Bhuvan <bhuonepartapsingh@gmail.com>
2024-03-20 12:40:26 +0530

    version 1.0.0.0

--- frontend/src/features/Projects/projectService.js
+++ frontend/src/features/Projects/projectService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = 'http://localhost:5000/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 // create new project
 const createProject = async (projectData, token) => {
